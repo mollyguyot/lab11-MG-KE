9a51263e771ac1005ad5b6a61ad7b8047c87f295
mollyguyot <mollyguyot@ufl.edu>
2025-11-12 08:40:28 -0500

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -31,27 +31,21 @@ class TestCalculator(unittest.TestCase):
 
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
-
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
-
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
-    
+    def test_divide_by_zero(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            divide(9, 0)
+    def test_logarithm(self): # 3 assertions
+        self.assertEqual(logarithm(100, 10), 2)
+        self.assertEqual(logarithm(2, 8), 3)
+        self.assertEqual(logarithm(5, 625), 4.0)
+    def test_log_invalid_base(self): # 1 assertion
+        with self.asserRaises(ValueError):
+            logarithm(1, 25)    
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
-
+    def test_log_invalid_argument(self): # 1 assertion
+        with self.asserRaises(ValueError):
+            logarithm(0, 5)
+            
     def test_hypotenuse(self): # 3 assertions  
         self.assertEqual(hypotenuse(3, 4), 5)
         self.assertEqual(hypotenuse(5, 12), 13)
